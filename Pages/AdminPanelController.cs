d6c22c073f3ae78fc350e5ac83db8bfe87705e13
Kuba Wichliński <kubawich45@gmail.com>
2018-02-23 15:48:16 +0100

    Webpack support added

---
+++ Pages/AdminPanelController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NSFWpics.Pages
+{
+    public class AdminPanelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,11 @@
-﻿$(document).ready(function () {
+﻿//Use it to build webpack ./node_modules/.bin/webpack ./wwwroot/js/site.js ./wwwroot/js/bundle.js
+$(document).ready(function () {
     $('.materialboxed').materialbox();
 })
 
+var moment = require('moment');
+console.log("Hello from JavaScript!");
+console.log(moment().startOf('day').fromNow());
 
 var app = new Vue({
     el: '#app',
