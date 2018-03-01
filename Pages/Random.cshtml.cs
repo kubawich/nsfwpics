41d08cc2e982006b7dcfb3bc266d7c5701cf0dec
Kuba Wichliński <kubawich45@gmail.com>
2018-03-01 17:08:12 +0100

    Added rest of blank pages like random and best etc.

---
+++ Pages/Random.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+
+namespace NSFWpics.Pages
+{
+    public class RandomModel : PageModel
+    {
+        Random rand = new Random();
+
+        public MySqlConnectionStringBuilder connection = new MySqlConnectionStringBuilder();
+        public int Id { get; private set; }
+        public string Con { get; private set; }
+        [BindProperty]
+        public Image Image { get; set; }
+
+        [HttpGet]
+        public IActionResult OnGet()
+        {
+            var i = rand.Next(1, 10500);
+
+            connection.Server = "185.28.102.194";
+            connection.UserID = "root";
+            connection.Password = "Kubawich1";
+            connection.Database = "content";
+            connection.SslMode = MySqlSslMode.None;
+
+            MySqlConnection conn = new MySqlConnection(connection.ToString());
+            MySqlCommand cmd; 
+
+            cmd = new MySqlCommand($"SELECT uri, author, date, id, points from imgs where id={i}", conn);
+            conn.Open();
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+               Image = new Image
+                {
+                    Id = int.Parse(reader["id"].ToString()),
+                    Uri = reader["uri"].ToString(),
+                    Author = reader["author"].ToString(),
+                    Points = int.Parse(reader["points"].ToString()),
+                    Date = reader["date"].ToString()
+                };
+            }
+
+            conn.Close();
+            return Page();
+        }
+    }
+}
---
+++ Pages/aditional/about.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NSFWpics.Pages.aditional
+{
+    public class aboutModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
