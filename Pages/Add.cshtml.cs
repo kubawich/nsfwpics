956622e35b76f06af26c8e0edf37ee040e669523
Kuba Wichliński <kubawich45@gmail.com>
2018-03-18 23:07:23 +0100

    Add page backend logic

--- Pages/Add.cshtml.cs
+++ Pages/Add.cshtml.cs
@@ -1,16 +1,111 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+using Renci.SshNet;
 
 namespace NSFWpics.Pages
 {
     public class AddModel : PageModel
     {
+        MySqlConnectionStringBuilder connection = new MySqlConnectionStringBuilder();
+        [BindProperty]
+        public IFormFile Upload { get; set; }
+        [BindProperty]
+        public Image Image { get; set; }
+        [BindProperty]
+        public string UploadOption { get; set; }
+        public int Id { get; set; }
+        private IHostingEnvironment _environment;
+
+        public AddModel(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+
         public void OnGet()
         {
+        }
+
+        public void OnPost()
+        {
+            #region SQL_server_connection
+            connection.Server = "185.28.102.194";
+            connection.UserID = "root";
+            connection.Password = "Kubawich1";
+            connection.Database = "content";
+            connection.SslMode = MySqlSslMode.None;
+
+            MySqlConnection conn = new MySqlConnection(connection.ToString());
+            MySqlCommand cmd;
+
+            cmd = new MySqlCommand($"SELECT id FROM imgs WHERE id ORDER BY id DESC LIMIT 1;", conn);
+
+            conn.Open();
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Id = int.Parse(reader["id"].ToString());
+            }
+            int MaxId = Id;
+
+            conn.Close();
+            #endregion
+            if (Request.Form["UploadOption"] == "1")
+            {
+                #region PHOTOS_UPLOAD
+                //Upload photo to server
+                using (SftpClient client = new SftpClient("185.28.102.194", 22, "root", "Kubawich1"))
+                {
+                    client.Connect();
+                    client.ChangeDirectory("/var/www/html/img");
+
+                    using (FileStream fs = new FileStream(Path.GetFileName(Upload.FileName), FileMode.Create))
+                    {
+                        client.BufferSize = 4 * 1024;
+                        client.UploadFile(Upload.OpenReadStream(), $"{MaxId + 1}.png");
+                    }
+                    client.Disconnect();
+                    client.Dispose();
+                }
+                //Add uploaded photo to SQL database
+                cmd = new MySqlCommand($"INSERT INTO imgs(uri,author,points) values('http://cdn.nsfwpics.pw/img/{MaxId + 1}.png','Anonymous',0)", conn);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                #endregion
+            }
+            else if (Request.Form["UploadOption"] == "2")
+            {
+                #region PHOTOS_videos
+                //Upload photo to server
+                using (SftpClient client = new SftpClient("185.28.102.194", 22, "root", "Kubawich1"))
+                {
+                    client.Connect();
+                    client.ChangeDirectory("/var/www/html/img");
+
+                    using (FileStream fs = new FileStream(Path.GetFileName(Upload.FileName), FileMode.Create))
+                    {
+                        client.BufferSize = 4 * 1024;
+                        client.UploadFile(Upload.OpenReadStream(), $"{MaxId + 1}.png");
+                    }
+                    client.Disconnect();
+                    client.Dispose();
+                }
+                //Add uploaded photo to SQL database
+                cmd = new MySqlCommand($"INSERT INTO imgs(uri,author,points) values('http://cdn.nsfwpics.pw/img/{MaxId + 1}.png','Anonymous',0)", conn);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                #endregion
+            }
         }
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -5,10 +5,18 @@ $(document).ready(function () {
     $('select').material_select();
 });
 
+let content = () => {
+    for (var i = 0; i < document.getElementsByName('photo'); i++) {
+        console.log(i);
+    }
+}
+console.log(content);
+
 var app = new Vue({
     el: '#app',
     data: {
-        points: 0
+        points: 0,
+        posts: []
     },
 
     watch: {
