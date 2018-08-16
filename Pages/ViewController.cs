1dd304eb467fbf52848e024200bc6ae9e56281b4
Kuba Wichliński <kubawich45@gmail.com>
2018-08-16 22:08:32 +0200

    API view img

---
+++ Pages/ViewController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace NSFWpics.Pages
+{
+    [Route("api/[controller]")]
+    public class ViewController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        Image image = new Image();
+        [HttpGet("{id}")]
+        public IEnumerable<string> Get(int id)
+        {
+            image.Author = NSFWpics.DBEntities.DBEntity.Instance.View(id, image).Author;
+            image.Date = NSFWpics.DBEntities.DBEntity.Instance.View(id, image).Date;
+            image.Id = NSFWpics.DBEntities.DBEntity.Instance.View(id, image).Id;
+            image.Points = NSFWpics.DBEntities.DBEntity.Instance.View(id, image).Points;
+            image.Uri = NSFWpics.DBEntities.DBEntity.Instance.View(id, image).Uri;
+            return new string[] { image.Author.ToString(), image.Date.ToString(), image.Id.ToString(), image.Points.ToString(), image.Uri.ToString() };
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
