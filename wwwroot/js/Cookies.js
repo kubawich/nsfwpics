6f22522592fd5f575a6d27aef3cd7a3c0441f233
Kuba Wichliński <kubawich45@gmail.com>
2018-10-07 14:52:02 +0200

    User cookies

---
+++ wwwroot/js/Cookies.js
@@ -0,0 +1,32 @@
+﻿//Checks if cookie exists
+function getCookie(cname) {
+	var name = cname + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+
+	//Check if loged in cookie is set
+	if (getCookie("user_loged_in") == "true") {
+		console.log("user_loged_in")
+		document.getElementById("login_nav_big").innerText = getCookie("login");
+		document.getElementById("login_nav_small").innerText = getCookie("login");
+		document.getElementById("add_nav_big").innerHTML = `<a asp-page="/Add">Add</a>`;
+		document.getElementById("add_nav_small").innerHTML = `<a asp-page="/Add" class="waves-effect waves-light red lighten-3 btn-small">Add</a>`;
+	}
+	else {
+		console.log(`no cookie`)
+		document.getElementById("add_nav_big").innerHTML = null;
+		document.getElementById("add_nav_small").innerHTML = null;
+	}
+
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -57,35 +57,3 @@ function minus(img_id, callback) {
 }
 
 
-//Checks if cookie exists
-function getCookie(cname) {
-	var name = cname + "=";
-	var decodedCookie = decodeURIComponent(document.cookie);
-	var ca = decodedCookie.split(';');
-	for (var i = 0; i < ca.length; i++) {
-		var c = ca[i];
-		while (c.charAt(0) == ' ') {
-			c = c.substring(1);
-		}
-		if (c.indexOf(name) == 0) {
-			return c.substring(name.length, c.length);
-		}
-	}
-	return "";
-}
-
-document.onload = checkLogin;
-
-function checkLogin() {
-	//Check if loged in cookie is set
-	if (getCookie("user_loged_in") == "true") {
-		document.getElementById("login_nav_big").innerText = getCookie("login");
-		document.getElementById("login_nav_small").innerText = getCookie("login");
-		document.getElementById("add_nav_big").innerHTML = `<a asp-page="/Add">Add</a>`;
-		document.getElementById("add_nav_small").innerHTML = `<a asp-page="/Add" class="waves-effect waves-light red lighten-3 btn-small">Add</a>`;
-	}
-	else if (getCookie("user_loged_in") != "true") {
-		document.getElementById("add_nav_big").innerHTML = null;
-		document.getElementById("add_nav_small").innerHTML = null;
-	}
-}
