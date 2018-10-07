da237e8e354fda176ec94748c4c3af8b737c3832
Kuba Wichliński <kubawich45@gmail.com>
2018-10-07 15:10:55 +0200

    JS

--- wwwroot/js/Cookies.js
+++ wwwroot/js/Cookies.js
@@ -15,18 +15,18 @@ function getCookie(cname) {
 	return "";
 }
 
-
-	//Check if loged in cookie is set
-	if (getCookie("user_loged_in") == "true") {
-		console.log("user_loged_in")
-		document.getElementById("login_nav_big").innerText = getCookie("login");
-		document.getElementById("login_nav_small").innerText = getCookie("login");
-		document.getElementById("add_nav_big").innerHTML = `<a asp-page="/Add">Add</a>`;
-		document.getElementById("add_nav_small").innerHTML = `<a asp-page="/Add" class="waves-effect waves-light red lighten-3 btn-small">Add</a>`;
-	}
-	else {
-		console.log(`no cookie`)
-		document.getElementById("add_nav_big").innerHTML = null;
-		document.getElementById("add_nav_small").innerHTML = null;
-	}
+//Check if loged in cookie is set
+if (getCookie("user_loged_in") == "true") {
+	console.log("user_loged_in");
+	document.getElementById("login_nav_big").innerText = getCookie("login");
+	document.getElementById("add_nav_big").innerHTML = `<a asp-page="/Add">Add</a>`;
+	document.getElementById("add_nav_small").innerHTML = `<a asp-page="/Add" class="waves-effect waves-light red lighten-3 btn-small">Add</a>`;
+}
+else {
+	console.log(`no cookie`);
+	document.getElementById("add_nav_big").innerHTML = null;
+	document.getElementById("add_nav_big").remove();
+	document.getElementById("add_nav_small").innerHTML = null;
+	document.getElementById("add_nav_small").remove();
+}
 
